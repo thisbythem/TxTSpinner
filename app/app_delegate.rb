6b19b1d2d97f4c5dc57888c7f7ca8ee894216e26
James Wagner <james@thisbythem.com>
2014-04-01 22:53:10 -0700

    Initial commit

---
+++ app/app_delegate.rb
@@ -0,0 +1,10 @@
+class AppDelegate
+  def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.backgroundColor = UIColor.whiteColor
+    @window.rootViewController = MainViewController.new
+    @window.makeKeyAndVisible
+
+    true
+  end
+end
---
+++ app/main_view_controller.rb
@@ -0,0 +1,30 @@
+class MainViewController < UIViewController
+
+  def viewDidLoad
+    super
+
+    logo_image = UIImage.imageNamed('txt-logo')
+
+    @logo_view = UIImageView.alloc.init
+    @logo_view.userInteractionEnabled = true
+    @logo_view.image = logo_image
+    @logo_view.size = [logo_image.size.width, logo_image.size.height]
+    @logo_view.center = self.view.center
+
+    @logo_view.when_tapped { logo_view_was_tapped }
+
+    self.view.addSubview @logo_view
+  end
+
+  def logo_view_was_tapped
+    timing = CAMediaTimingFunction.send('functionWithControlPoints::::', 0.28, -0.3, 0.12, 1.0)
+
+    rotateAnimation = CABasicAnimation.animationWithKeyPath("transform.rotation")
+    rotateAnimation.toValue = Math::PI / 180 * 720
+    rotateAnimation.duration = 2.0
+    rotateAnimation.timingFunction = timing
+
+    @logo_view.layer.addAnimation(rotateAnimation, forKey: :logo_spinning)
+  end
+
+end
